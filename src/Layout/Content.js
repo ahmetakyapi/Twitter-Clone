e985863c53ac9b142cee4d1afb25ecbc805db6ed
ahmetakyapi <ahmetakyapii@gmail.com>
2021-05-11 23:12:42 +0300

    Sidebar ve Content ikonları oluşturuldu.

--- src/Layout/Content.js
+++ src/Layout/Content.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {PopulerIcon} from "../icons/icons";
+import {ImageIcon} from "../icons/icons";
+import {GifIcon} from "../icons/icons";
+import {PollIcon} from "../icons/icons";
+import {EmojiIcon} from "../icons/icons";
+import {ScheduleIcon} from "../icons/icons";
+import {ReplyIcon} from "../icons/icons";
+import {RetweetIcon} from "../icons/icons";
+import {LikeIcon} from "../icons/icons";
+import {ShareIcon} from "../icons/icons";
+import {SearchIcon} from "../icons/icons";
 
 const Content = () => {
         return (
             <main className="flex-1 bg-gray-dark">
               <h1>Content</h1>
+                <PopulerIcon className="w-7 h-7"/>
+                <ImageIcon className="w-7 h-7"/>
+                <GifIcon className="w-7 h-7"/>
+                <PollIcon className="w-7 h-7"/>
+                <EmojiIcon className="w-7 h-7"/>
+                <ScheduleIcon className="w-7 h-7"/>
+                <ReplyIcon className="w-7 h-7"/>
+                <RetweetIcon className="w-7 h-7"/>
+                <LikeIcon className="w-7 h-7"/>
+                <ShareIcon className="w-7 h-7"/>
+                <SearchIcon className="w-7 h-7"/>
             </main>
         )
     }
--- src/Layout/Sidebar.js
+++ src/Layout/Sidebar.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import {HomeIcon} from "../icons/icons";
+import {ExploreIcon} from "../icons/icons";
+import {NotificationsIcon} from "../icons/icons";
+import {MessagesIcon} from "../icons/icons";
+import {BookmarksIcon} from "../icons/icons";
+import {ListsIcon} from "../icons/icons";
+import {ProfileIcon} from "../icons/icons";
+import {MoreIcon} from "../icons/icons";
+
 
 const Sidebar = () => {
     return (
         <div className="w-72 bg-blue-200">
             <h1>Sidebar</h1>
+            <HomeIcon/>
+            <ExploreIcon/>
+            <NotificationsIcon/>
+            <MessagesIcon/>
+            <BookmarksIcon/>
+            <ListsIcon/>
+            <ProfileIcon/>
+            <MoreIcon/>
         </div>
     )
 }
-
-
 
 export default Sidebar
