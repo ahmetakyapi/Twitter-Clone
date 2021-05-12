4b75bca1919dfdfada6c7511a41ce60c3ccd56a6
ahmetakyapi <ahmetakyapii@gmail.com>
2021-05-12 17:35:06 +0300

    Update Content.js

--- src/Layout/Content.js
+++ src/Layout/Content.js
@@ -1,16 +1,5 @@
 import React, {useEffect} from 'react';
-import db from '../firebase'
 import {PopulerIcon} from "../icons/icons";
-import {ImageIcon} from "../icons/icons";
-import {GifIcon} from "../icons/icons";
-import {PollIcon} from "../icons/icons";
-import {EmojiIcon} from "../icons/icons";
-import {ScheduleIcon} from "../icons/icons";
-import {ReplyIcon} from "../icons/icons";
-import {RetweetIcon} from "../icons/icons";
-import {LikeIcon} from "../icons/icons";
-import {ShareIcon} from "../icons/icons";
-import {SearchIcon} from "../icons/icons";
 
 const Content = () => {
         return (
