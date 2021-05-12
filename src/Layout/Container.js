fae52604ee08f8dba9b07a4ba8ee77e38e27c260
ahmetakyapi <ahmetakyapii@gmail.com>
2021-05-12 17:42:02 +0300

    Vercel için düzenlemeler.

--- src/Layout/Container.js
+++ src/Layout/Container.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className="flex min-h-screen max-w-7xl mx-auto bg-primary-base border max-w-4xl mx-auto px-6 bg-white  ">
+        <div className="flex min-h-screen max-w-7xl mx-auto bg-primary-base border max-w-4xl mx-auto px-6 bg-white">
             {children}
         </div>
     )
--- src/Layout/Content.js
+++ src/Layout/Content.js
@@ -3,7 +3,7 @@ import {PopulerIcon} from "../icons/icons";
 
 const Content = () => {
         return (
-            <main className="bg-white flex-1 flex flex-col border-r border-l border-gray-extraLight">
+            <main className="bg-white flex-1 flex flex-col border-r border-l border-gray-extraLight max-w-4xl mx-auto px-6">
               <header className="sticky top-0 z-10 flex justify-between items-center p-4 border-b border-gray-extraLight bg-white">
                   <span className="font-bold text-xl text-gray-900">Home</span>
                   <PopulerIcon className="w-7 h-7 text-primary-base"/>
