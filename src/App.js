255102aa1a24183ac886f7a8db905c624e4dc71f
ahmetakyapi <ahmetakyapii@gmail.com>
2021-05-18 02:06:46 +0300

    Update.Widgets

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Widgets from "./Layout/Widgets";
 
 const App = () => {
    return (
-       <Container>
-        <Sidebar/>
-        <Content/>
-        <Widgets/>
-       </Container>
+       <div className="max-w-6xl mx-auto px-6 py-2">
+           <Container>
+               <Sidebar/>
+               <Content/>
+               <Widgets/>
+             </Container>
+           )
+       </div>
+
    )
 }
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+            screens: {
+                'phone': '1024px',
+                // => @media (min-width: 1024px) { ... }
+
+                'desktop': '1280px',
+                // => @media (min-width: 1280px) { ... }
+            },
         extend: {
             colors: {
                 primary: {
