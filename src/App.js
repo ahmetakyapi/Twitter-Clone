89c5c0ebb9350498923b0609d1a9ee4b3fc0ae52
ahmetakyapi <ahmetakyapii@gmail.com>
2021-05-18 02:13:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Widgets from "./Layout/Widgets";
 
 const App = () => {
    return (
-       <div className="max-w-6xl mx-auto px-6 py-2">
+       <div className="max-w-6xl mx-auto px-14 py-2">
            <Container>
                <Sidebar/>
                <Content/>
